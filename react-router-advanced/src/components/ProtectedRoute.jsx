823494401ab39d27d8e9baeba330b34aeac9b79b
Bamikiya Precious Ayomide <133870138+SirBamz@users.noreply.github.com>
2025-08-24 23:36:46 +0100

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,14 @@
 import { Navigate } from 'react-router-dom';
+// import your custom useAuth hook
+import useAuth from '../hooks/useAuth';
 
-function ProtectedRoute({ isAuthenticated, children }) {
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
   return children;
 }
-
 
 export default ProtectedRoute;
