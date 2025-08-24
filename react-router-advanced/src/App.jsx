aa2318ed0026c06d71b7c7519df87b2a02ab9986
Bamikiya Precious Ayomide <133870138+SirBamz@users.noreply.github.com>
2025-08-24 23:39:35 +0100

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,13 +4,16 @@ import Profile from './pages/Profile';
 import BlogPost from './pages/BlogPost';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/profile/*" element={<ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>} />
         <Route path="/blog/:id" element={<BlogPost />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
