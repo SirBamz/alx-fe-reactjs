69c0f1325cd9431fdfe2e4779663b4d71da84448
Bamikiya Precious Ayomide <133870138+SirBamz@users.noreply.github.com>
2025-08-24 23:27:25 +0100

    Add files via upload

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import BlogPost from './pages/BlogPost';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
