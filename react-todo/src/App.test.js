05bc60d2b41c16482f55e4035f58930f01295193
Bamikiya Precious Ayomide <133870138+SirBamz@users.noreply.github.com>
2025-08-30 23:51:27 +0100

    Add files via upload

--- react-todo/src/App.test.js
+++ react-todo/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Todo List heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/todo list/i);
+  expect(heading).toBeInTheDocument();
 });
