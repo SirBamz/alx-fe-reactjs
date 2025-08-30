089f08b95231539521e35689b0f56cd919dd4585
Bamikiya Precious Ayomide <133870138+SirBamz@users.noreply.github.com>
2025-08-31 00:36:14 +0100

    Delete react-todo/src directory

--- react-todo/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Todo List heading', () => {
-  render(<App />);
-  const heading = screen.getByText(/todo list/i);
-  expect(heading).toBeInTheDocument();
-});
